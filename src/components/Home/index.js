386e2c0518a53296ba1d9d0545b94f62d0054a7a
Tushar Deepak <tushardeepak22@gmail.com>
2022-04-01 18:50:18 +0530

    update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,12 +5,35 @@ import "./home.css";
 
 function Home() {
   const { mainData } = useDataContext();
+  const [search, setSearch] = React.useState("");
   return (
     <div className="homeContainer">
+      <input
+        value={search}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setSearch(e.target.value);
+        }}
+        className="searchBox"
+        placeholder="Search"
+      />
       <div className="homeBox">
-        {mainData?.map((d) => (
-          <Cards key={d.id} data={d} />
-        ))}
+        {mainData
+          ?.filter((d) => {
+            if (search === "") return d;
+            else if (d.name.toLowerCase().includes(search.toLowerCase())) {
+              return d;
+            } else if (d.status.toLowerCase().includes(search.toLowerCase())) {
+              return d;
+            } else if (d.gender.toLowerCase().includes(search.toLowerCase())) {
+              return d;
+            } else if (d.species.toLowerCase().includes(search.toLowerCase())) {
+              return d;
+            }
+          })
+          .map((d) => (
+            <Cards key={d.id} data={d} />
+          ))}
       </div>
     </div>
   );
--- src/components/LikePage/index.js
+++ src/components/LikePage/index.js
@@ -5,10 +5,33 @@ import "./likepage.css";
 
 function LikePage() {
   const { mainData } = useDataContext();
+  const [search, setSearch] = React.useState("");
   return (
     <div className="likepageContainer">
+      <input
+        value={search}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setSearch(e.target.value);
+        }}
+        className="searchBox"
+        placeholder="Search"
+      />
       <div className="likepageBox">
-        {mainData?.map((d) => (d.liked ? <Cards key={d.id} data={d} /> : ""))}
+        {mainData
+          ?.filter((d) => {
+            if (search === "") return d;
+            else if (d.name.toLowerCase().includes(search.toLowerCase())) {
+              return d;
+            } else if (d.status.toLowerCase().includes(search.toLowerCase())) {
+              return d;
+            } else if (d.gender.toLowerCase().includes(search.toLowerCase())) {
+              return d;
+            } else if (d.species.toLowerCase().includes(search.toLowerCase())) {
+              return d;
+            }
+          })
+          ?.map((d) => (d.liked ? <Cards key={d.id} data={d} /> : ""))}
       </div>
     </div>
   );
